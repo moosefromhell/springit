86b108d5493bc65a053089f09d99f42394175754
joshuaarcher <joshua.archer94@gmail.com>
2022-02-28 17:48:35 -0700

    added logger

--- src/main/java/com/springit/springit/SpringitApplication.java
+++ src/main/java/com/springit/springit/SpringitApplication.java
@@ -1,14 +1,36 @@
 package com.springit.springit;
 
+import com.springit.springit.config.SpringitProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
 		System.out.println("Welcome to SpringIt!");
 	}
 
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			log.error("CommandLineRunner.run();");
+			log.warn("CommandLineRunner.run();");
+			log.info("CommandLineRunner.run();");
+			log.debug("CommandLineRunner.run();");
+			log.trace("CommandLineRunner.run();");
+		};
+	}
 }
