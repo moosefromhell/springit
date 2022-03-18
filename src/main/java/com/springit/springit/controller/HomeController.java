253fda550bfee7b16a240f407e04cefa138676e8
joshuaarcher <joshua.archer94@gmail.com>
2022-03-18 17:29:00 -0600

    added link controller

---
+++ src/main/java/com/springit/springit/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.springit.springit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(Model model, HttpServletRequest request){
+        model.addAttribute("message", "Hello Lips!");
+        return "index";
+    }
+
+}
